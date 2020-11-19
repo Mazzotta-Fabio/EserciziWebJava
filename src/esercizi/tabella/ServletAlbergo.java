7cbc70e2afe53776ce8a12e340b5a679b7eecb2e
Mazzotta-Fabio <f.mazzotta3@studenti.unisa.it>
2020-11-19 21:40:03 +0100

    modifiche al javascript esercizio rating

--- src/esercizi/tabella/ServletAlbergo.java
+++ src/esercizi/tabella/ServletAlbergo.java
@@ -1,15 +1,11 @@
 package esercizi.tabella;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.stream.XMLEventWriter;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamWriter;
 
 import esercizi.bean.GestioneStruttura;
 import esercizi.bean.Struttura;
